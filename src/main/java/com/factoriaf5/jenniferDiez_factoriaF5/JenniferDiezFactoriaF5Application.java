fdabeccf900b575461687439c12b5b1b4723dfbc
Jennifer D. <jennifer.diezs@gmail.com>
2025-01-26 20:09:45 +0100

    feat: initial project setup
    -Created the Spring Boot project.
    -Configured app properties.
    -Added basic dependencies.

---
+++ src/main/java/com/factoriaf5/jenniferDiez_factoriaF5/JenniferDiezFactoriaF5Application.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.jenniferDiez_factoriaF5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenniferDiezFactoriaF5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenniferDiezFactoriaF5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/factoriaf5/jenniferDiez_factoriaF5/JenniferDiezFactoriaF5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.jenniferDiez_factoriaF5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenniferDiezFactoriaF5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

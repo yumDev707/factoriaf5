720c0f253692ad8a858199d4bf784887fb3c0d79
Jennifer D. <jennifer.diezs@gmail.com>
2025-01-28 15:29:20 +0100

    Add functionality to upload a new image.
    - Added a new modal component for the "Add Image" feature.
    - Implemented a new method in the controller to handle image uploads.
    - Images are now being saved correctly in the database and file system.

--- src/main/java/com/factoriaf5/jenniferDiez_factoriaF5/controllers/IndexController.java
+++ src/main/java/com/factoriaf5/jenniferDiez_factoriaF5/controllers/IndexController.java
@@ -1,19 +1,26 @@
 package com.factoriaf5.jenniferDiez_factoriaF5.controllers;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.factoriaf5.jenniferDiez_factoriaF5.services.ImagesService;
 import com.factoriaf5.jenniferDiez_factoriaF5.valueObjects.ImagesVO;
 
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/index")
 public class IndexController {
 	@Autowired
 	ImagesService imgSrv;
@@ -22,6 +29,28 @@ public class IndexController {
 	public String index(Model model) {
 		List<ImagesVO> totalImages = imgSrv.findAll();
 		model.addAttribute("totalImages", totalImages);
+		
+		model.addAttribute("image", new ImagesVO());
 		return "index";
+	}
+
+	@PostMapping("/addImage")
+	public String addImage(@ModelAttribute ImagesVO img, @RequestParam("imageFile") MultipartFile imageFile) {
+	    String directory = "src/main/resources/static/images/gallery/";
+	    
+	    try {
+	        if (!imageFile.isEmpty()) {
+	            String fileName = imageFile.getOriginalFilename();
+	            Path filePath = Paths.get(directory, fileName);
+	            imageFile.transferTo(filePath);
+	            img.setUrl(fileName);
+	        }
+	        imgSrv.save(img);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        return "redirect:/index?error=true";
+	    }
+
+	    return "redirect:/index";
 	}
 }
